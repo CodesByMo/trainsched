087c959ccc3579dd75708d92870afe7fa209ba44
CodesByMo <morganspencer162@gmail.com>
2019-06-19 18:46:26 -0700

    finished, working. Adding styling later

--- assets/app.js
+++ assets/app.js
@@ -19,11 +19,47 @@ $(document).ready(function () {
     // var trainDiv = $("#trainInfo").append("#trainBody);
 
     var dbRecord;
-
     database.ref().on("child_added", function (snapshot) {
-        var name = snapshot.name;
-        // console.log(snapshot);
+        var nameObj = snapshot.val().name;
+        var destObj = snapshot.val().destination;
+        var timeObj = snapshot.val().nextArrival;
+        var freqObj = snapshot.val().frequency;
+        console.log(nameObj);
+        console.log(destObj);
+        console.log(timeObj);
+        console.log(freqObj);
 
+        var tr = $("<tr>");
+        var td1 = $("<td>").text(nameObj);
+        var td2 = $("<td>").text(destObj);
+        var td3 = $("<td>").text(timeObj);
+        var td4 = $("<td>").text(freqObj);
+
+        tr.append(td1);
+        tr.append(td2);
+        tr.append(td3);
+        tr.append(td4);
+
+        $("#trainBody").append(tr);
+
+
+
+        // console.log(Object.values(snapshot.val()));
+        // function displayData() {
+        // Object.values(snapshot.val())
+        // $("#trainBody").append("<tr><td>" + nameObj + "</td>");
+        // $("#trainBody").append("<td>" + destObj + "</td>");
+        // $("#trainBody").append("<td>" + timeObj + "</td");
+        // $("#trainBody").append("<td>" + freqObj + "</td></tr>");
+        // console.log("child");
+        // }
+        // displayData();
+        // for (i = 0; i < 4; i++) {
+        //     Object.values(snapshot.val());
+        //     Object.values(snapshot.val());
+        //     Object.values(snapshot.val());
+        //     Object.values(snapshot.val());
+        // }
     });
 
     ;
@@ -39,10 +75,10 @@ $(document).ready(function () {
         var nextArrival = $("#trainTime").val();
         var frequency = $("#trainFreq").val();
 
-        console.log(name);
-        console.log(destination);
-        console.log(nextArrival);
-        console.log(frequency);
+        // console.log(name);
+        // console.log(destination);
+        // console.log(nextArrival);
+        // console.log(frequency);
 
         database.ref().push({
             name: name,
@@ -56,20 +92,20 @@ $(document).ready(function () {
         $("#trainTime").val("");
         $("#trainFreq").val("");
 
-        function populateTable() {
-            // clear table
-            $("#trainBody").empty();
-            // loop through data
+        // function populateTable() {
+        // clear table
+        // $("#trainBody").empty();
+        // loop through data
 
-            // append <TR> to <TBODY>
-            $("#trainBody").append("<td>" + name + "</td>");
-            $("#trainBody").append("<td>" + destination + "</td>");
-            $("#trainBody").append("<td>" + nextArrival + "</td>");
-            $("#trainBody").append("<td>" + frequency + "</td>");
+        // append <TR> to <TBODY>
+        // $("#trainBody").append("<td>" + name + "</td>");
+        // $("#trainBody").append("<td>" + destination + "</td>");
+        // $("#trainBody").append("<td>" + nextArrival + "</td>");
+        // $("#trainBody").append("<td>" + frequency + "</td>");
 
-        }
+        // }
 
-        populateTable();
+        // populateTable();
 
 
     });
@@ -86,27 +122,27 @@ $(document).ready(function () {
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-    console.log(firstTimeConverted);
+    // console.log(firstTimeConverted);
 
     // Current Time
     var currentTime = moment();
-    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // console.log("DIFFERENCE IN TIME: " + diffTime);
 
     // Time apart (remainder)
     var tRemainder = diffTime % tFrequency;
-    console.log(tRemainder);
+    // console.log(tRemainder);
 
     // Minute Until Train
     var tMinutesTillTrain = tFrequency - tRemainder;
-    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
 
 
@@ -114,31 +150,38 @@ $(document).ready(function () {
     // Using .on("value", function(snapshot)) syntax will retrieve the data
     // from the database (both initially and every time something changes)
     // This will then store the data inside the variable "snapshot". We could rename "snapshot" to anything.
-    database.ref().on("value", function (snapshot) {
+    // database.ref().on("value", function (snapshot) {
 
-        // Then we console.log the value of snapshot
-        // console.log(snapshot.val());
+    //     // Then we console.log the value of snapshot
+    //     // console.log(snapshot.val());
 
-        var name = $("#addTrainName").val();
-        var destination = $("#trainDest").val();
-        var nextArrival = $("#trainTime").val();
-        var frequency = $("#trainFreq").val();
+    //     var name = $("#addTrainName").val();
+    //     var destination = $("#trainDest").val();
+    //     var nextArrival = $("#trainTime").val();
+    //     var frequency = $("#trainFreq").val();
 
-        $("#trainBody").append("<td>" + name + "</td>");
-        $("#trainBody").append("<td>" + destination + "</td>");
-        $("#trainBody").append("<td>" + nextArrival + "</td>");
-        $("#trainBody").append("<td>" + frequency + "</td>");
+    //     // $("#trainBody").text(name);
+    //     // $("#trainBody").text(destination);
+    //     // $("#trainBody").text(nextArrival);
+    //     // $("#trainBody").text(frequency);
 
-        // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
-        // Again we could have named errorObject anything we wanted.
-        console.log(snapshot);
+    //     // snapshot.key.name[0];
 
-    }, function (errorObject) {
+    //     // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+    //     // Again we could have named errorObject anything we wanted.
+    //     // console.log(snapshot.val());
+    //     // console.log(snapshot.name);
 
-        // In case of error this will print the error
-        console.log("The read failed: " + errorObject.code);
-        database.ref().push(snapshot);
+    //     // var loadSnap = JSON.stringify(snapshot);
 
-    });
+
+    // }, function (errorObject) {
+
+    //     // In case of error this will print the error
+    //     // console.log("The read failed: " + errorObject.code);
+    //     database.ref().push(snapshot);
+
+    // });
 
 })
+
